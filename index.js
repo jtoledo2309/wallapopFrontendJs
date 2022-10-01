b2583b72f2834e8ef89d3c7574706d93be40359b
Juan Toledo <>
2022-10-01 15:56:16 +0200

    Inicio subir producto

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+import { NotificationController } from "./NotificationController.js";
 import { ProductListController } from "./products-list/ProductListController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const notificationElement = document.querySelector("#notification");
   const productListElement = document.querySelector("#product-list");
 
+  const handleUserLogged = () => {
+    const token = localStorage.getItem("token");
+    const userActions = document.querySelector("user-actions");
+    if (token) {
+      userActions.innerHTML = '<a href="">Subir producto</a>';
+    } else {
+      userActions.innerHTML = '<a href="./signup.html">Inicia sesión</a>';
+    }
+  };
+
+  const notificationController = new NotificationController(
+    notificationElement
+  );
   const productListController = new ProductListController(productListElement);
+
+  handleUserLogged();
 });
